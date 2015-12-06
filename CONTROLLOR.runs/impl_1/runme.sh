7c116fe51ca65a93dcbdd8e5e5a99a047a5d3217
maicuong <mai.cuong.fs1991@gmail.com>
2015-12-06 22:16:10 +0900

    json TEXT_INPUT_VHDL OK

--- CONTROLLOR.runs/impl_1/runme.sh
+++ CONTROLLOR.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log CONTROLLOR_VHDL.vdi -applog -m64 -messageDb vivado.pb -mode batch -source CONTROLLOR_VHDL.tcl -notrace
 
 
--- CONTROLLOR.srcs/sources_1/imports/CONTROLLOR/TEXT_INPUT_VHDL.vhd
+++ CONTROLLOR.srcs/sources_1/imports/CONTROLLOR/TEXT_INPUT_VHDL.vhd
@@ -14,44 +14,128 @@ end TEXT_INPUT_VHDL;
 architecture Behavioral of TEXT_INPUT_VHDL is
 
 begin
-	process(CLK)
+	process
        file text_input : text is in "C:\FPGAPrj\CONTROLLOR\text_input.txt";
        variable l:         line;
-       variable c:         character;
-       variable is_string: boolean;	 
+       variable c,c_read:         character;
+       variable is_string: boolean;
+       variable str : string(1 to 100) := (others => ' ');	 
+       variable next_sig : std_logic := '0';
+       variable j,n:         natural := 1;
+       variable end_sig : boolean := false;
     begin
 	if(CLK'event and CLK = '1') then
-	if(endfile(text_input)) then
-		char_out <= ESC;
-	else	
-	   while not endfile(text_input) loop	
-        readline(text_input, l);
-        for i in 1 to 100 loop
-           --wait until CLK = '1';
-	       if (TRG = '1' or RDY = '1') then
-	       --read(l, c, is_string);
-	       --while c = ' ' loop
-	       --read(l, c, is_string);
-	       --end loop;
-	       
-	       
-	       for i in 1 to 10 loop
-             read(l, c, is_string);
-	       exit when c /= ' ';
-	       end loop;
-
-           if is_string then 
-		      char_out <= c;
-		      STR_OUT <= STR_OUT(2) & c;
-           else 
-	          char_out <= LF;
-	          str_out <= str_out(2)&LF;
-	       exit;
-           end if;
-           end if;
-         end loop;
-		end loop;
-	end if;
+      if not endfile(text_input) then	
+                    --if(TRG = '1' or RDY = '1') then
+                    if(TRG = '1' or next_sig = '1') then
+                  readline(text_input, l);
+                        j := 1;
+                        for i in str'range loop
+                            str(i) := ' ';
+                       end loop;  
+                          for i in str'range loop
+                        read(l, c, is_string);
+                        if is_string then 
+                            if(c /= ' ') then
+                            str(j) := c;
+                            j := j + 1;
+                            end if;
+                            --test(i) <= c;
+                            --test <= str(256);
+                        else 
+                        str(j) := LF;
+                        exit;
+                        end if;
+                    
+                    end loop;
+                  
+                  next_sig := '0';
+                  n := 1;
+                  end if;
+                  else
+                    --for i in str'range loop
+                        --    str(i) := ' ';
+                       --end loop;
+                    --str(1) := ESC;
+                    --next_sig := '0';
+                  --n := 1;
+                  end_sig := true;
+                  end if;
+                 -- clear the contents of the result string
+                 --for i in text_string_array(j)'range loop
+                     --text_string_array(j)(i) := ' ';
+                 --end loop;   
+                 -- read all characters of the line, up to the length  
+                 -- of the results string
+                  --wait until CLK = '1'; 
+                  --next_sig := true;
+                  --while next_sig ;
+                 --for i in text_string_array(j)'range loop
+                    --wait until CLK = '1';
+                  --if(next_sig = '1') then
+                    --next_sig <= '0';
+                  --end if;
+                    
+                if((TRG = '1' or RDY = '1')) then
+                --trg_sig <= next_sig or CLK;
+                
+                --if (TRG = '1' or RDY = '1') then
+                 --while(c = ' ') loop
+                 --if (next_sig) then
+                 
+                 --wait until (TRG = '1' or RDY = '1') ;
+                 
+                 --for i in 1 to 100 loop
+                --read(l, c_read, is_string);
+                   --num <= i ;
+                    if(n > 0 and n <100) then
+                    if(not(c_read = LF and end_sig)) then 
+                    c_read := str(n);
+                    end if;
+                    n := n + 1;
+                    end if;
+                   --exit when (c_read /= ' ');
+                 --if(c = ' ') then next;
+                 --else
+                 --next_sig := false;
+                 
+                 --end if;
+                 --end loop;
+                 
+                    if(c_read = LF) then
+                        next_sig := '1';
+                    end if;
+                 
+                 --n := i ;
+                --if is_string then 
+                    --text_string_array(j)(i) := c;
+                    --res_string_array(j)(i) <= c;
+                    if(c_read = LF and end_sig) then
+                    char_out <= ESC;
+                    else
+                    char_out <= c_read;
+                    end if;
+                    --str_out_sig(1) := str_out_sig(2);
+                    --str_out_sig(2) := c;
+                    STR_OUT <= STR_OUT(2) & c_read;
+                    --RDY <= '0';
+                    --CONTINUE <= '0';
+                    --next_sig <= '0';
+                --else 
+                 --next_sig <= '1';
+                 --char_out <= LF;
+                 --str_out <= str_out(2)&LF;
+                 --next_sig := '1';
+                --end if;
+               --end if;
+                    
+                    
+                --end if;
+                 --end loop;
+                    
+                    --if(j < LINE_NUMBER) then
+                    --j := j+1;
+                    end if;
 	end if;
 end process;
 
